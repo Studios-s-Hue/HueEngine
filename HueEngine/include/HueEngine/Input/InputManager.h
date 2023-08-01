18604b3ed169cdbb2982568c8438f25c8a05ea0a
Ran-j <ran-junior@hotmail.com>
2023-08-01 15:58:52 -0300

    feat: added input manager

---
+++ HueEngine/include/HueEngine/Input/InputManager.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <HueEngine\CoreMinimal.h>
+#include <HueEngine\Input\InputKeysType.h>
+#include <array>
+#include <unordered_map>
+
+class InputManager {
+public:
+    InputManager();
+
+    bool IsKeyDown(const EKey& key) const;
+    bool IsKeyUp(const EKey& key) const;
+
+    bool IsMouseDown(const EMouseButton& button) const;
+    bool IsMouseUp(const EMouseButton& button) const;
+
+    bool IsJoystickButtonDown(const EJoystickButton& button) const;
+    float GetJoystickAxis(ui32 axis) const;
+
+    float GetMouseXAxis() const;
+    float GetMouseYAxis() const;
+
+    void SetInputEnable(bool enable);
+
+    // void SetScreenArea(const Rect& area);
+
+    bool IsInputEnable() const { return m_inputEnable; }
+
+public:
+    void Update();
+
+private:
+    std::unordered_map<EKey, bool> m_keyState;
+    std::unordered_map<EMouseButton, bool> m_mouseButtonState;
+    std::unordered_map<EJoystickButton, bool> m_joystickButtonState;
+    float m_joystickAxisCount[2];
+    
+    double m_mouseX;
+    double m_mouseY;
+    // Rect m_screenArea;
+    bool m_inputEnable;
+};
---
+++ HueEngine/source/HueEngine/Input/InputManager.cpp
@@ -0,0 +1,126 @@
+#include <HueEngine/Input/InputManager.h>
+#include <GLFW/glfw3.h>
+
+InputManager::InputManager() : m_inputEnable(true) {}
+
+bool InputManager::IsJoystickButtonDown(const EJoystickButton &button) const
+{
+    if (m_inputEnable)
+    {
+        return m_joystickButtonState.at(button);
+    }
+    return false;
+}
+
+float InputManager::GetJoystickAxis(ui32 axis) const
+{
+    if (m_inputEnable)
+    {
+        return m_joystickAxisCount[axis];
+    }
+
+    return 0.0f;
+}
+
+bool InputManager::IsKeyDown(const EKey &key) const
+{
+    if (m_inputEnable)
+    {
+        return m_keyState.at(key);
+    }
+
+    return false;
+}
+
+bool InputManager::IsKeyUp(const EKey &key) const
+{
+    if (m_inputEnable)
+    {
+        return !m_keyState.at(key);
+    }
+
+    return false;
+}
+
+bool InputManager::IsMouseDown(const EMouseButton &button) const
+{
+    if (m_inputEnable)
+    { 
+        return m_mouseButtonState.at(button);
+    }
+
+    return false;
+}
+
+bool InputManager::IsMouseUp(const EMouseButton &button) const
+{
+    if (m_inputEnable)
+    {
+        return !m_mouseButtonState.at(button);
+    }
+
+    return false;
+}
+
+void InputManager::SetInputEnable(bool enable)
+{
+    m_inputEnable = enable;
+}
+
+void InputManager::Update()
+{
+    auto window = nullptr; // TODO: Get window from somewhere
+    if (m_inputEnable)
+    {
+        m_mouseButtonState[EMouseButton::Left] = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
+        m_mouseButtonState[EMouseButton::Right] = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS;
+        m_mouseButtonState[EMouseButton::Middle] = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS;
+
+        int firstKey = static_cast<int>(EKey::A);
+        int lastKey = static_cast<int>(EKey::Tab);
+
+        for (int i = firstKey; i <= lastKey; ++i)
+        {
+            auto key = static_cast<EKey>(i);
+            auto keyExistInMap = m_keyState.find(key);
+            if (keyExistInMap != m_keyState.end())
+            {
+                m_keyState[key] = glfwGetKey(window, i) == GLFW_PRESS;
+            }
+        }
+
+        int buttonCount;
+        const unsigned char *buttonStates = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
+
+        if (buttonStates != nullptr)
+        {
+            for (int i = 0; i < buttonCount; ++i)
+            {
+                m_joystickButtonState[static_cast<EJoystickButton>(i)] = buttonStates[i] == GLFW_PRESS;
+            }
+        }
+
+        int AxesCount;
+        const float *axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &AxesCount);
+
+        if (axes != nullptr)
+        {
+            for (int i = 0; i < AxesCount; ++i)
+            {
+                m_joystickAxisCount[i] = axes[i];
+            }
+        }
+
+        glfwGetCursorPos(window, &m_mouseX, &m_mouseY);
+    }
+}
+
+float InputManager::GetMouseXAxis() const
+{
+    return static_cast<float>(m_mouseX);
+}
+
+float InputManager::GetMouseYAxis() const
+{
+    return static_cast<float>(m_mouseY);
+}
